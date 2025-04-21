78b69b075435cbf0fd48f284dcd941e298bf6fc5
405862_BARRIONUEVO_SANTIAGO <santiagobarrionuevo9@gmail.com>
2025-04-21 20:37:04 -0300

    Commit
    Creación del proyecto

---
+++ Bailaconmigo/src/main/java/org/example/bailaconmigo/BailaconmigoApplication.java
@@ -0,0 +1,13 @@
+package org.example.bailaconmigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BailaconmigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BailaconmigoApplication.class, args);
+	}
+
+}
---
+++ Bailaconmigo/src/test/java/org/example/bailaconmigo/BailaconmigoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bailaconmigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BailaconmigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
